ea4afe661acc1aa861e6a2b68e2cdbb3f15d4d7c
shyamsundar624 <shyamsundaryadav624@gmail.com>
2024-10-22 22:54:06 +0530

    import code added

--- Excel-Export/src/main/java/com/shyam/excel/controller/StudentController.java
+++ Excel-Export/src/main/java/com/shyam/excel/controller/StudentController.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.shyam.excel.service.StudentService;
 
@@ -35,5 +39,16 @@ public class StudentController {
 				headers(headers).
 				contentType(MediaType.APPLICATION_OCTET_STREAM).
 				body(inputStream.readAllBytes());
+	}
+	
+	@PostMapping("/import")
+	public ResponseEntity<String> importStudentInf(@RequestParam("file") MultipartFile file){
+		try {
+			studentService.importStudentInfo(file);
+			return ResponseEntity.status(HttpStatus.CREATED).body("Student data Import Successfully");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Issue");
+		}
 	}
 }
--- Excel-Export/src/main/java/com/shyam/excel/service/StudentService.java
+++ Excel-Export/src/main/java/com/shyam/excel/service/StudentService.java
@@ -1,16 +1,27 @@
 package com.shyam.excel.service;
 
+import java.awt.Color;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFDataFormat;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.shyam.excel.entity.Student;
 import com.shyam.excel.repo.StudentRepository;
@@ -22,47 +33,111 @@ public class StudentService {
 
 	@Autowired
 	private StudentRepository studentRepository;
-	
+
 	@PostConstruct
 	public void saveStudent() {
-		List<Student> list = List.of(
-				new Student("shyam", "CS", "Axis", 3432),
-				new Student("sangam", "CS", "Axis", 3432),
-				new Student("Navaya", "CS", "Axis", 3432));
+		List<Student> list = List.of(new Student("shyam", "CS", "Axis", 3432),
+				new Student("sangam", "CS", "Axis", 3432), new Student("Navaya", "CS", "Axis", 3432));
 
 		studentRepository.saveAll(list);
 	}
-	
+
 	public ByteArrayInputStream exportStudentData() throws IOException {
 		List<Student> all = studentRepository.findAll();
-		
-		ByteArrayOutputStream out=new ByteArrayOutputStream();
-		
-		XSSFWorkbook workbook=new XSSFWorkbook();
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		XSSFWorkbook workbook = new XSSFWorkbook();
 		XSSFSheet sheet = workbook.createSheet("Student");
-		
+
 		XSSFRow headRow = sheet.createRow(0);
-		XSSFCell cell = headRow.createCell(0);
-		cell.setCellValue("Id");
-		
-		headRow.createCell(1).setCellValue("Name");
-		headRow.createCell(2).setCellValue("Branch");
-		headRow.createCell(3).setCellValue("College");
-		headRow.createCell(4).setCellValue("Fees");
-		
-		int rowIndex=1;
-		for(Student s:all) {
+
+		XSSFCellStyle headerStyle = workbook.createCellStyle();
+		XSSFColor color = new XSSFColor(new Color(204, 255, 204), null);
+		headerStyle.setFillForegroundColor(color);
+		headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		int headerCellIndex = 0;
+		String[] headerArr = { "Id", "Name", "Branch", "College", "Fees" };
+		for (String header : headerArr) {
+			XSSFCell cell = headRow.createCell(headerCellIndex);
+			cell.setCellValue(header);
+			cell.setCellStyle(headerStyle);
+			headerCellIndex++;
+		}
+		XSSFCellStyle decimalStyle = workbook.createCellStyle();
+		XSSFDataFormat format = workbook.createDataFormat();
+		decimalStyle.setDataFormat(format.getFormat("0.00"));
+
+		int rowIndex = 1;
+		for (Student s : all) {
 			XSSFRow row = sheet.createRow(rowIndex);
-			
+
 			row.createCell(0).setCellValue(s.getId());
 			row.createCell(1).setCellValue(s.getName());
 			row.createCell(2).setCellValue(s.getBranch());
 			row.createCell(3).setCellValue(s.getCollege());
-			row.createCell(4).setCellValue(s.getFees());
+			XSSFCell cellFees = row.createCell(4);
+			cellFees.setCellValue(s.getFees());
+			cellFees.setCellStyle(decimalStyle);
 			rowIndex++;
 		}
-workbook.write(out);
-return new ByteArrayInputStream(out.toByteArray());
+		workbook.write(out);
+		return new ByteArrayInputStream(out.toByteArray());
 	}
-	
+
+	public void importStudentInfo(MultipartFile file) throws IOException {
+		InputStream inputStream = file.getInputStream();
+		XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+		for (Sheet sheet : workbook) {
+			for (Row row : sheet) {
+				if (row.getRowNum() > 0) {
+					Student student = new Student();
+					for (Cell cell : row) {
+						String cellValue = "";
+						 // Get the cell type
+				        CellType cellType = cell.getCellType();
+				        
+				        switch (cellType) {
+				            case STRING:
+				                // If the cell contains a string value
+				                System.out.println("Cell contains a String: " + cell.getStringCellValue());
+				               cellValue=String.valueOf(cell.getStringCellValue());
+				                break;
+				            case NUMERIC:
+				                // If the cell contains a numeric value
+				                System.out.println("Cell contains a Numeric value: " + cell.getNumericCellValue());
+				               cellValue=String.valueOf(cell.getNumericCellValue());
+				                break;
+				            case BOOLEAN:
+				                // If the cell contains a boolean value
+				                System.out.println("Cell contains a Boolean value: " + cell.getBooleanCellValue());
+				                break;
+				            case FORMULA:
+				                // If the cell contains a formula
+				                System.out.println("Cell contains a Formula: " + cell.getCellFormula());
+				                break;
+				            case BLANK:
+				                System.out.println("Cell is blank.");
+				                break;
+				            default:
+				                System.out.println("Cell type not recognized.");
+				        }
+					
+						if (cell.getColumnIndex() == 1) {
+							student.setName(cellValue);
+						} else if (cell.getColumnIndex() == 2) {
+							student.setBranch(cellValue);
+						} else if (cell.getColumnIndex() == 3) {
+							student.setCollege(cellValue);
+						} else if (cell.getColumnIndex() == 4) {
+							student.setFees(Double.valueOf(cellValue));
+						}
+					}
+					studentRepository.save(student);
+				}
+			}
+		}
+	}
+
 }
